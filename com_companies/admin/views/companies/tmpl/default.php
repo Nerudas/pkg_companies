f597132221e91d43342f90d356fb59fabc5c0ee5
Septdir <septdir@gmail.com>
2018-04-24 14:06:07 +0300

    Add filtersHidden

--- com_companies/admin/views/companies/tmpl/default.php
+++ com_companies/admin/views/companies/tmpl/default.php
@@ -37,8 +37,8 @@ $columns = 11;
 
 <form action="<?php echo Route::_('index.php?option=com_companies&view=companies'); ?>" method="post" name="adminForm"
 	  id="adminForm">
-
-	<?php echo LayoutHelper::render('joomla.searchtools.default', array('view' => $this)); ?>
+	<?php echo LayoutHelper::render('joomla.searchtools.default',
+		array('view' => $this, 'options' => array('filtersHidden' => false))); ?>
 	<?php if (empty($this->items)) : ?>
 		<div class="alert alert-no-items">
 			<?php echo Text::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
