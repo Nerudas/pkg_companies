a9b99595723cbdbbd493b88356a05e1970ae4c39
Septdir <septdir@gmail.com>
2018-05-16 16:47:51 +0300

    Change version to 1.0.6

--- mod_companies_admin/media/css/default.css
+++ mod_companies_admin/media/css/default.css
@@ -1,6 +1,6 @@
 /*
  * @package    Companies - Administrator Module
- * @version    1.0.5
+ * @version    1.0.6
  * @author     Nerudas  - nerudas.ru
  * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
