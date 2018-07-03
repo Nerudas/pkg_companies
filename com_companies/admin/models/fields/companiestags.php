23a6a377e0603cdce8321696469f8270d9a7b06e
Septdir <septdir@gmail.com>
2018-07-03 16:36:58 +0300

    Add companies tags

---
+++ com_companies/admin/models/fields/companiestags.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * @package    Companies Component
+ * @version    1.0.10
+ * @author     Nerudas  - nerudas.ru
+ * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
+ * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ * @link       https://nerudas.ru
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Form\FormHelper;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Language\Text;
+
+FormHelper::loadFieldClass('list');
+
+class JFormFieldCompaniesTags extends JFormFieldList
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var    string
+	 * @since  1.0.0
+	 */
+	protected $type = 'companiesTags';
+
+	/**
+	 * Method to get the field options.
+	 *
+	 * @return  array  The field option objects.
+	 *
+	 * @since  1.0.0
+	 */
+	protected function getOptions()
+	{
+		$params  = ComponentHelper::getParams('com_companies');
+		$tags    = $params->get('tags');
+		$options = parent::getOptions();
+
+		// Load languages
+		$language = Factory::getLanguage();
+		$language->load('com_companies', JPATH_ADMINISTRATOR, $language->getTag(), true);
+
+		// Root
+		$root        = new stdClass();
+		$root->text  = Text::_($params->get('root_title', 'COM_INFO'));
+		$root->value = 1;
+		if ($this->value == $root->value)
+		{
+			$root->selected = true;
+		}
+		$options[] = $root;
+
+		if (!empty($tags) && is_array($tags))
+		{
+			// Get tags
+			$db    = Factory::getDbo();
+			$query = $db->getQuery(true)
+				->select(array('t.id', 't.title'))
+				->from($db->quoteName('#__tags', 't'))
+				->where($db->quoteName('t.alias') . ' <>' . $db->quote('root'))
+				->where('t.id IN (' . implode(',', $tags) . ')')
+				->order($db->escape('t.lft') . ' ' . $db->escape('asc'));
+			$db->setQuery($query);
+			$objects = $db->loadObjectList();
+
+			foreach ($objects as $i => $tag)
+			{
+				$option        = new stdClass();
+				$option->text  = $tag->title;
+				$option->value = $tag->id;
+				if ($option->value == $this->value)
+				{
+					$option->selected = true;
+				}
+				$options[] = $option;
+			}
+		}
+
+		return $options;
+	}
+}
