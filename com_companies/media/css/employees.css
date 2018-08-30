0981452d06137da0918c4a1d8ff61f81fe3f2824
Septdir <septdir@gmail.com>
2018-08-30 12:44:43 +0300

    Change version to 1.2.2

--- com_companies/media/css/employees.css
+++ com_companies/media/css/employees.css
@@ -1,6 +1,6 @@
 /*
  * @package    Companies Component
- * @version    1.2.1
+ * @version    1.2.2
  * @author     Nerudas  - nerudas.ru
  * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
--- plg_system_companies_shortcodes/companies_shortcodes.php
+++ plg_system_companies_shortcodes/companies_shortcodes.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package    System - Companies Shortcodes Plugin
- * @version    1.2.1
+ * @version    1.2.2
  * @author     Nerudas  - nerudas.ru
  * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
